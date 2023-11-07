4ad1acf23066c172ad9030726cc4d53f2473a3c5
= <=>
2023-11-07 00:01:03 -0300

    Inserted canvasText pseudo-object

--- js/scriptdev.js
+++ js/scriptdev.js
@@ -7,20 +7,35 @@
     let backgroundDraw = new Image();
     backgroundDraw.src = '../images/fundo_sieski.jpg';
 
+   //pseudo-object - DrawText
+    const canvasText = function (positionX,positionY,Text,Color) {
+        this.text = Text;
+        this.font = "52px Arial Bold";
+        this.color = Color;
+        this.originX = positionX;
+        this.originY = positionY;
+        this.render = function () {
+            cnv.font = this.font;
+            cnv.fillStyle = this.color;
+            cnv.fillText(this.text, this.originX, this.originY);
+    };
+};
+
     //pseudo-object
     const canvasBackground = function (Background, Width, Height) {
         this.width = Width;
         this.height = Height;
         this.originX = 0;
         this.originY = 0;
-        this.render = function () {
+        this.render = function () {            
             cnv.drawImage(Background, this.originX, this.originY, this.width, this.height);
         };
     };
 
     //Begin Code
     const canvasBackImage = new canvasBackground(backgroundDraw, 1450, 780);
-
+    const infoText = new canvasText(450, 400,"Bem vindo ao canvas" ,"yellow");
+    
     window.onload = init();
 
     //Link html canvas element
@@ -37,6 +52,7 @@
     function start() {
         backgroundDraw.onload = function () {
             canvasBackImage.render();
+            infoText.render();
         };
     };
 
@@ -49,7 +65,7 @@
     function draw() {
         cnv.clearRect(0, 0, 700, 450);
         canvasBackImage.render();
-        //cnv.drawImage(background, 0, 0, background.width, background.height);
+        infoText.render();
     }
 
     //RECURSIVE / LOOP 
